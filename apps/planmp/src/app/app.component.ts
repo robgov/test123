c697f70155e7e1a36f0d997b0b46766875d2efc9
Jeff Hantke <jeff.hantke@gov.ab.ca>
2021-05-03 13:37:53 -0600

    Initial Push

---
+++ apps/planmp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AngularComponentsModule } from '@abgov/angular-components';
+
+@Component({
+  selector: 'aedigital-mono-repo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'planmp';
+}
