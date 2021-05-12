dc17ac4015fa9d113628ab8d4195c03466924aa2
Robert Barnard <robert.barnard@gov.ab.ca>
2021-05-12 17:01:46 -0600

    missing the routing for navigation.

--- interface/apps/planmp/src/app/app.module.ts
+++ interface/apps/planmp/src/app/app.module.ts
@@ -16,7 +16,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
-  imports: [BrowserModule, AngularComponentsModule, FlexLayoutModule, MatIconModule, MatButtonModule],
 
   declarations: [AppComponent, TombstoneComponent, HomeComponent, HeaderComponent],
   imports: [BrowserModule, AngularComponentsModule, FlexLayoutModule, 
