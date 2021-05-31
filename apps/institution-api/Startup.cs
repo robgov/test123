a57559761ed102d87b3563148cbc9484dcaa229b
Andrew Mackenzie <andrew.mackenzie@gov.ab.ca>
2021-05-31 13:22:30 -0600

    updated yaml file for institution-api

--- apps/institution-api/Startup.cs
+++ apps/institution-api/Startup.cs
@@ -19,6 +19,8 @@ namespace ProviderApi
         Configuration = configuration;
     }
 
+// Startup test 
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
