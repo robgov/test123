ccdacfce4bae436bc3e638ee5eb01f9f0c2e4bc5
Sam.Kothari <sam.kothari@gov.ab.ca>
2021-06-07 19:50:46 +0000

    Update app.po.ts

--- apps/planmp-e2e/src/support/app.po.ts
+++ apps/planmp-e2e/src/support/app.po.ts
@@ -1,9 +1,6 @@
 export const getGreeting = () => cy.get('h1');
 
 export const injectAxe = () => {
-    //cy.injectAxe();
-    // cy.injectAxe is currently broken. https://github.com/component-driven/cypress-axe/issues/82
-    // Creating our own injection logic
     cy.readFile('../../node_modules/axe-core/axe.min.js').then((source) => {
       return cy.window({ log: false }).then((window) => {
         window.eval(source);
