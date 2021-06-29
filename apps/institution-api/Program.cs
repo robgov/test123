d2150eeb2cf39c01cdbf82cff24a8af128ad78b1
Robert Barnard <robert.barnard@gov.ab.ca>
2021-06-29 08:51:11 -0600

    wip

--- apps/institution-api/Program.cs
+++ apps/institution-api/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Azure.Identity;
 
 namespace ProviderApi
 {
@@ -18,6 +19,21 @@ namespace ProviderApi
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
       Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+    if (context.HostingEnvironment.IsDevelopment())
+    {
+
+      // Do simply nothing
+    }
+    else
+    {
+      var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+      config.AddAzureKeyVault(
+      keyVaultEndpoint,
+      new DefaultAzureCredential());
+    }
+})
         .ConfigureWebHostDefaults(webBuilder =>
         {
           webBuilder.UseStartup<Startup>();
