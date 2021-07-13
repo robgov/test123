88fc0c0839ed1e58757561518ceac46a42fc83b9
Jeff Hantke <jeff.hantke@gov.ab.ca>
2021-07-13 09:17:37 -0600

    Fix term and credential

--- apps/planmp/src/app/programs/program-summary-detail/program-summary-detail.component.ts
+++ apps/planmp/src/app/programs/program-summary-detail/program-summary-detail.component.ts
@@ -30,4 +30,12 @@ export class ProgramSummaryDetailComponent implements OnInit {
     return "MacEwan University inspires students with a powerful combination of academic excellence and personal learning experiences - ours is a connected culture that focuses on learner-centred teaching, and on providing opportunities for students to grow and achieve. The university provides a transformative education in a collaborative and supportive environment where creativity and innovation thrive. Faculties focus on teaching, and many are also engaged in ground-breaking, internationally-recognized research and scholarly and artistic activity that enhances their teaching and the curriculum - and creates opportunities for student involvement in research. The university offers a broad complement of programs comprised of bachelor and applied degrees, diplomas and certificates. Faculties and schools offer programming in fine arts and communications; health and community studies; liberal arts and sciences; and business.";
   }
 
+  getProgramTermandCredential(): string {
+    if (!this.program.programCredential) return '';
+    return (
+      this.program.programLength +
+      ' - ' +
+      this.program.programCredential
+    );
+  }
 }
